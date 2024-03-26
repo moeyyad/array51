118b42fae6ff7cce1f30a1f6cfdd0ad794e352db
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2024-03-26 15:29:20 -0400

    deploy on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://moeyyad.github.io",
+  base: "courses",
   integrations: [tailwind(), mdx(), sitemap()],
 });
