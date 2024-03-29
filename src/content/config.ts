c4626aff875d5a3f490b80a595f74c1293b62191
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2024-03-29 08:52:28 -0400

    landing page, contact form, programs page, courses page

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,8 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
+
+const courseCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
@@ -18,22 +19,9 @@ const blogCollection = defineCollection({
   }),
 });
 
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
-});
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  'courses': courseCollection
 };
