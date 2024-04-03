de7d89f5181de91a72945d3deb83e225d4bb9794
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2024-04-03 16:23:17 -0400

    changed project name to array51

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://moeyyad.github.io",
-  base: "prosper",
+  base: "array51",
   integrations: [tailwind(), mdx(), sitemap()],
 });
